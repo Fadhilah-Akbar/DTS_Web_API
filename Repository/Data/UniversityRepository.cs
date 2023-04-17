3402255a56c51597c612a2062f8571eee515051a
Fadhilah Akbar <69349706+Fadhilah-Akbar@users.noreply.github.com>
2023-04-17 13:16:29 +0700

    Add files via upload

---
+++ Repository/Data/UniversityRepository.cs
@@ -0,0 +1,20 @@
+﻿using DTS_Web_Api.Contexts;
+using DTS_Web_Api.Models;
+using DTS_Web_Api.Repository.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace DTS_Web_Api.Repository.Data
+{
+    public class UniversityRepository : GeneralRepository<TbMUniversity, int, MyContext>, IUniversityRepository
+    {
+        public UniversityRepository(MyContext context) : base(context)
+        {
+        }
+
+        public async Task<bool> IsNameExist(string name)
+        {
+            var entity = await _context.Set<TbMUniversity>().FirstOrDefaultAsync(x => x.Name == name);
+            return entity != null;
+        }
+    }
+}
